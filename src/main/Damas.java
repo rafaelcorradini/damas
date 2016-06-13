2d7e4e7536c5aa3f2c12bff5563a44011e07da3a
Rafael Corradini da Cunha <rafacunhadini@gmail.com>
2016-06-13 02:43:45 -0300

    class Damas e Jogador

--- src/main/Damas.java
+++ src/main/Damas.java
@@ -1,4 +1,64 @@
 package main;
 class Damas {
+	private int[][] tabuleiro = new int[8][8]; // 0 - vazio, 1 - peça preta, 2 - peça branca, 3 - dama preta e 4 - dama branca
+	private int tamanho = 8;
 	
+	/**
+	 * Método construtor, atribui 8 para o tamanho do tabuleiro.
+	 * Tabuleiro 8x8, 64 casas.
+	 * O tamanho não poderá ser alterado posteriormente. 
+	 */
+	public Damas() {
+		this.tamanho = tamanho;
+		tabuleiro = new int[tamanho][tamanho];	
+	}
+	
+	/**
+	 * Método construtor, atribui o tamanho do tabuleiro que deverá ser passado por parâmetro.
+	 * Ex: 8 para um tabuleiro 8x8, 64 casas.
+	 * O tamanho não poderá ser alterado posteriormente. 
+	 * @param tamanho Tamanho do tabuleiro.
+	 */
+	public Damas(int tamanho) {
+		this.tamanho = tamanho;
+		tabuleiro = new int[tamanho][tamanho];	
+	}
+	
+	/**
+	 * Retorna o tamanho do tabuleiro, a quantidade de casas que tem uma linha do tabuleiro.
+	 * Ex: 8 para um tabuleiro 8x8, 64 casas.
+	 * @return Inteiro com o tamanho do tabuleiro.
+	 */
+	public int getTamanho() {
+		return this.tamanho;
+	}
+	
+	/**
+	 * Retorna uma matriz que representa o tabuleiro
+	 * 0 representa uma casa vazia, 1 uma peça branca, 2 uma peça preta, 3 uma dama branca e 4 uma dama preta.
+	 * @return Retorna uma matriz de duas dimensões de inteiros.
+	 */
+	public int[][] getTabuleiro() {
+		return this.tabuleiro;
+	}
+	
+	/**
+	 * 
+	 * @param fromI
+	 * @param fromJ
+	 * @param toI
+	 * @param toJ
+	 * @return Retorna True caso a jogada seja feita, False caso não seja possível fazer a jogada.
+	 */
+	public boolean fazerJogada(int fromI, int fromJ, int toI, int toJ, Jogador jogador) {
+		final int from = tabuleiro[fromI][fromJ], to = tabuleiro[toI][toJ]; 
+		
+		if((from == 3 || from == 4) || (from == 1 && fromI < toI) || (from == 2 && fromI > toI)) { // aq
+			
+		} else {
+			return false;
+		}
+		
+		
+	}
 }
---
+++ src/main/Jogador.java
@@ -0,0 +1,20 @@
+package main;
+
+public class Jogador {
+	private String nome;
+	private int cor; // 1 - preto, 2 - branco
+	
+	/**
+	 * Método construtor, atribui nome do jogador e a cor das peças do mesmo.
+	 * @param nome Nome do jogador
+	 * @param cor A cor das peças do jogador, 1 para preto e 2 para branco.
+	 */
+	public Jogador(String nome, int cor) {
+		this.nome = nome;
+		this.cor = cor;
+	}
+	
+	public int getCor() {
+		return cor;
+	}
+}
