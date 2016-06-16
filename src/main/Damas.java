71de167d28df6fdf7a399461ac1c54f38504a0b2
Rafael Corradini da Cunha <rafacunhadini@gmail.com>
2016-06-15 21:00:44 -0300

    melhorJogada(), melhorJogadaAux()

--- src/main/Damas.java
+++ src/main/Damas.java
@@ -1,6 +1,6 @@
 package main;
 class Damas {
-	private int[][] tabuleiro = new int[8][8]; // 0 - vazio, 1 - peça preta, 2 - peça branca, 3 - dama preta e 4 - dama branca
+	private int[][] tabuleiro = new int[8][8]; // -1 - não é casa, 0 - vazio, 1 - peça preta, 2 - peça branca, 3 - dama preta e 4 - dama branca
 	private int[][] tabuleiroTemp = new int[8][8];
 	private int pontosJogada = 0; // pontos acumulados na jogada atual.
 	private int tamanho = 8;
@@ -13,6 +13,7 @@ class Damas {
 	 */
 	public Damas() {
 		tabuleiro = new int[tamanho][tamanho];	
+		initTabuleiro(tamanho);
 	}
 	
 	/**
@@ -24,6 +25,24 @@ class Damas {
 	public Damas(int tamanho) {
 		this.tamanho = tamanho;
 		tabuleiro = new int[tamanho][tamanho];	
+		initTabuleiro(tamanho);
+	}
+	
+	/**
+	 * Inicializa o tabuleiro colocando as peças em seus devidos lugares
+	 * @param tamanho Tamanho do tabuleiro.
+	 */
+	public void initTabuleiro(int tamanho) {
+		for(int i = 0; i < tamanho; i++) {
+			for(int j = 0; j < tamanho; j++) {
+				if ((i == 0 && j == 0) || (i+j) % 2 == 0)
+					tabuleiro[i][j] = -1;
+				else if (i < ((tamanho-2) / 2))
+					tabuleiro[i][j] = 1;
+				else if (i > ((tamanho-2) / 2) + 1)
+					tabuleiro[i][j] = 2;
+			}
+		}
 	}
 	
 	/**
@@ -158,7 +177,7 @@ class Damas {
 						}
 					} 
 				} else {
-					for (int i = fromI, j = fromJ; i != toI || j != toJ; i--, j--) { // terminar
+					for (int i = fromI, j = fromJ; i != toI || j != toJ; i--, j--) { 
 						if (tabuleiroTemp[i][j] > 0) {
 							if (tabuleiroTemp[i-1][j-1] > 0 || (i-1 != toI && j-1 != toJ)) return -1;
 							if (jogador.getCor() == 1) {
@@ -191,13 +210,12 @@ class Damas {
 	}
 	
 	public int melhorJogada(Jogador jogador) {
-		final int cor = jogador.getCor();
 		int[][] tabuleiroT = tabuleiroTemp;
 		
 		for (int i = 0; i < tamanho; i++) {
 			for (int j = 0; j < tamanho; j++) {
 				if(tabuleiro[i][j] > 0)
-					melhorJogadaAux(i, j, jogador, 0);
+					melhorJogadaAux(i, j, jogador, -1);
 			}
 		}
 		
@@ -210,7 +228,7 @@ class Damas {
 		
 		if(melhorJogada < cont+1) melhorJogada = cont+1;
 		
-		if (tabuleiro[i][j] < 3) {
+		if (tabuleiroTemp[i][j] < 3) {
 			if (fazerMovimento(i, j, i-2, j+2, jogador) == 1)
 				melhorJogadaAux(i-2, j+2, jogador, cont+1);
 			
@@ -223,9 +241,26 @@ class Damas {
 			if (fazerMovimento(i, j, i+2, j-2, jogador) == 1)
 				melhorJogadaAux(i+2, j-2, jogador, cont+1);
 				
-		} else if (tabuleiro[i][j] > 2) {
-			// terminar
+		} else if (tabuleiroTemp[i][j] > 2) {
+			int diag = (i+j) / 2; 
+			if (diag < tamanho/2) {
+				for (int u = diag*2+1, v = 0; u >= 0; u--, v++) {
+					if (tabuleiroTemp[u][v] > 0)
+						if (fazerMovimento(i, j, u, v, jogador) == 1)
+							melhorJogadaAux(u, v, jogador, cont+1);
+				}	
+				
+			} else {
+				for (int u = tamanho-1, v = diag*2; v < tamanho; u--, v++) {
+					if (tabuleiroTemp[u][v] > 0)
+						if (fazerMovimento(i, j, u, v, jogador) == 1)
+							melhorJogadaAux(u, v, jogador, cont+1);
+				}	
+			}
+				
 		}
+		
+		tabuleiroTemp = tabuleiroT;
 		
 
 	}
